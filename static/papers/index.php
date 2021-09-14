a3acef69066d4d354a1f1e97334e8abd3b3419c6
Sujit <S.K.Sahu@soton.ac.uk>
2021-09-14 16:24:11 +0100

    papers

---
+++ static/papers/index.php
@@ -0,0 +1,401 @@
+<?php
+ require("../my.php");
+  my_Header('/research/','Research of Sujit Sahu'); 
+?>
+
+
+
+
+
+</ol>
+
+<hr WIDTH="90%">
+
+
+
+
+<h1> <b>Papers </b></h1>
+
+<ol> 
+
+<br>
+<li> Bakar, K. S. and Sahu, S. K. (2012)
+<a href="papers/spTimeRpaper.pdf">spTimer: Spatio-Temporal Bayesian Modelling Using R. </a> <i> Technical Report, </i> University of Southampton, UK. 
+
+
+<li>  Sahu, S. K., Baffour, B., Harper, P. R., Minty, J. H. and 
+    Sarran, C.
+  (2013)
+<a href="papers/admissionmetsim.pdf">A Hierarchical Bayesian Model for Improving Short-Term Forecasting of Hospital Demand  by Including Meteorological Information. </a>   
+<i> Journal of the Royal Statistical Society, </i> Series A.  <i> To appear. </i>
+
+<li> Sahu, S. K. and Bakar, K. S. (2012)
+<a href="papers/sptimegpp.pdf">Hierarchical Bayesian auto-regressive models for large space 
+    time data with applications to ozone concentration modelling. </a> <i> Applied Stochastic Models in Business and Industry, </i> <b> 28, </b> 395-415.   
+
+
+<li> Gelfand, A. E., Sahu S. K.  and Holland, D. M.   (2012)
+  <a href="papers/distributedprefsampling.pdf"> On the Effect of Preferential Sampling in Spatial Prediction. </a>  <i> Environmetrics, </i>  <b> 23, </b> 565-578.   
+
+
+<li> Sahu, S. K. (2012)
+<a href="papers/sahu_handbook.pdf"> Hierarchical Bayesian models for space-time air pollution data </a>  In 
+<i>  Handbook of Statistics-Vol 30.  Time Series Analysis, 
+    Methods and Applications. </i> Editors: T Subba Rao and  C R Rao.
+    Elsevier Publishers, Holland.   Elsevier Publishers, Amsterdam, pp 477-495. 
+
+
+<li> Sahu, S. K. and Bakar, K. S. (2011)
+<a href="papers/sahubakar11.pdf"> A comparison of  Bayesian Models for Daily Ozone Concentration Levels
+    </a>  <i> Statistical Methodology </i>,  <b> 9, </b> 144-157,  DOI: 10.1016/j.stamet.2011.04.009. 
+
+<li> Sahu, S. K., Gelfand, A. E. and Holland, D. M.   (2010)
+<a href="papers/sahuetal2010.pdf"> 
+Fusing point and areal level space-time
+data with application to wet deposition. 
+</a>  <i> Journal of the Royal Statistical Society</i>, Series C, 
+Applied Statistics, <b> 59 </b>, 77-103. 
+
+
+<li>
+
+Sahu, S. K., Yip, S. and Holland, D. M.   (2009)
+<a href="papers/sahuyipholland_env_eco_09.pdf">
+A fast Bayesian method for updating and forecasting hourly ozone levels.
+</a> <i> Environmental and Ecological Statistics, </i>  <b> 18, </b> 185-207,
+ DOI 10.1007/s10651-009-0127-y.
+
+
+
+<li> Gelfand, A. E. and Sahu, S. K.   (2009)
+<a href="papers/gelfandsahu09.pdf"> Combining Monitoring Data and Computer model Output
+in Assessing Environmental Exposure.
+</a>  In <i> Handbook of Applied Bayesian Analysis </i> edited by 
+ Anthony O'Hagan  and Mike West, pp482-510. 
+
+
+
+<li> Sahu, S. K. and Chai, H. S. (2009)
+<a href="papers/chai.html"> 
+ A new skew-elliptical distribution and its properties. </a>
+ <i>  Calcutta Statistical Association Bulletin, </i> <b> 61, </b>
+197--225. 
+
+
+<li>
+
+Sahu, S. K., Yip, S. and Holland, D. M.   (2009)
+<a href="papers/sahuyipholland.pdf">
+ Improved space-time forecasting of next day ozone concentrations in the eastern U.S
+</a> <i> Atmospheric Environment, </i> <b> 43, </b>
+494-501.
+
+
+<li>  
+Sahu, S. K. and Nicolis, O. (2008) <a href="papers/sahunicolis.pdf"> An evaluation of European air pollution regulations for particulate matter monitored from a heterogeneous network.
+</a>  <i> Environmetrics,  </i>   <b> 20: </b> 943--961. 
+DOI:10.1002/env.965. </li>
+
+
+<li> Sahu, S. K. and Challenor, P.   (2008)
+<a href="papers/sahuchallenor.pdf"> 
+A space-time model for joint modeling of ocean temperature and salinity levels as measured by  Argo floats
+</a>  
+<i> Environmetrics, </i> <b> 19: </b> 509--528. 
+
+<li> Sahu, S. K., Gelfand, A. E. and Holland, D. M.   (2007)
+<a href="papers/pubozone.html"> 
+ High Resolution Space-Time Ozone
+Modeling for Assessing Trends. 
+</a>  
+<i> Journal of the American Statistical Association. </i> <b> 102</b>,
+1221--1234. 
+
+<li> Jona Lasinio, G.,  Sahu, S. K.   and  Mardia, K. V.  (2007)
+<a href="papers/varanasi.html"> Modeling rainfall data using a Bayesian Kriged-Kalman model.  </a>
+In <i> Bayesian Statistics and its Applocations</i>  edited by S. K. Upadhya, U. Singh and D. K. Dey. 
+Anshan Ltd. London.  
+
+
+<li> Sahu, S. K., Gelfand, A. E. and Holland, D. M.   (2006)
+<a href="papers/epa.html"> 
+Spatio-temporal modeling of fine particulate matter.  
+</a>  
+ <a href="http://jabes.unl.edu/index.htm"> 
+Journal of Agricultural, Biological, and Environmental Statistics.</a> 
+  <b> 11</b>, 61--86.
+
+
+
+<li> Sahu, S. K. and Smith, T. M. F. (2006)
+<a href="papers/SahuSmith06.pdf"> 
+A  Bayesian  method of 
+sample size determination  with practical applications </a>
+<i> Journal of the Royal Statistical Society</i>, Series A.</a>  
+ <b> 169</b>, 235--253.
+
+
+<li> Sahu S.K., Jona Lasinio G., Orasi A., and  Mardia, K.V. (2005). A Comparison
+of Spatio-Temporal Bayesian Models for Reconstruction of Rainfall Fields
+in a Cloud Seeding Experiment. <i> Journal of Mathematics and Statistics </i>
+<b> 1 </b> (4), pp. 273--281 ISSN: 1549-3644.
+
+
+
+<li> Sahu, S. K. and Mardia, K. V. (2005)
+<a href="papers/recent.html"> 
+Recent Trends in Modeling Spatio-Temporal Data. </a>
+In Proceedings of the special meeting on Statistics and Environment organized 
+ by the Societ\`{a} Italiana di Statistica held in Universit\`{a} Di Messina, 
+   September 21-23, 2005, <b> Invited Papers, </b> pages 69--83. Published 
+ by the  Universit\`{a} Di Messina, Messina, Italy. 
+
+
+<li>
+Sahu, S. K. and Mardia, K. V. (2005)
+<a href="papers/sahumardia05.pdf"> 
+ A Bayesian Kriged-Kalman model for short-term forecasting 
+of air pollution levels. </a>
+<i> Journal of the Royal Statistical 
+Society, Series C,  Applied Statistics, </i> 
+<b> 54, </b> 223--244. </li>
+
+
+<li>
+Sahu, S. K. and Dey, D. K. (2004)
+<a href="papers/logskew.html"> On  a Bayesian multivariate survival 
+models with a skewed frailty </a>
+In <i>  Skew-Elliptical Distributions and Their
+Applications: A Journey Beyond Normality, </i> 
+M. G. Genton (ed). CRC/Chapman & Hall, Boca Raton, FL, pp. 321-338 . 
+</li>
+
+ 
+
+<li>
+Sahu, S. K. (2004)
+<a href="papers/archmc.html"> 
+ Applications of formal model choice to archaeological 
+chronology building.
+ </a>
+In <i> Tools for Constructing Chronologies: Crossing Disciplinary Boundaries,
+</i> Buck, C.E. and Millard, A. R. (eds). London: Springer-Verlag. 
+pp 111--127.   
+</li> 
+
+
+
+
+<li>
+Sahu, S. K., Dey, D. K. and Branco, M. D. (2003) 
+<a href="papers/sahudeybranco.pdf"> A New Class of Multivariate 
+Skew Distributions with  Applications to Bayesian Regression Models.
+</a>
+<i> The Canadian Journal of Statistics. </i>  
+<b> 31 </b> 129--150.  
+</li>
+
+
+<li>
+Sahu, S. K. and Zhigljavsky, A. A. (2003)
+<a href="papers/self.html">Self Regenerative Markov Chain Monte Carlo with
+Adaptation.
+</a>
+ <i> Bernoulli. </i>   <b> 9, </b> 395--422.   
+</li>
+
+
+<li>
+Sahu, S. K. and Cheng, R. C. H. (2003)
+<a href="mixture/"> A Fast Distance Based Approach for Determining the 
+Number of Components in Mixtures </a>
+<i> The Canadian Journal of Statistics. </i>   
+<b> 31 </b> 3--22.  
+</li>
+
+
+
+
+<li>
+Sahu, S. K. (2002)
+<a href="papers/it.html">Bayesian Estimation and Model Choice in Item Response
+Models.</a> 
+<i> Journal of Statistical Computation and Simulation. </i>  
+<b> 72, </b> 217--232.  
+</li>
+
+
+
+<li> 
+Roberts, G. O. and Sahu, S. K. (2001) <a href="papers/dm.html"> 
+Approximate pre-determined convergence properties of the Gibbs sampler.  </a> 
+<i> Journal of Computational and Graphical Statistics. </i>
+<b> 10, </b> 216--229. </li>
+
+
+<li>
+Buck, C. E. and Sahu, S. K. (2000) <a href="papers/arch.html">
+Bayesian models for relative archaeological
+chronology building. </a> <i> Applied Statistics. </i>
+<b> 49, </b> 423--440 </li>
+
+<li>
+Sahu, S. K. and Dey, D. K. (2000) <a href="papers/comp.html">
+A Comparison of Frailty and Other Models for Bivariate Survival Data. 
+ </a> <i> Lifetime Data Analysis. </i>
+<b> 6 </b> 207--228. </li>
+
+
+<li>
+Sahu, S. K. and Roberts, G. O. (1999) <a href="papers/em.html"> 
+On Convergence of the EM Algorithm and the Gibbs Sampler. </a> 
+<i>Statistics and Computing. </i><b>9, </b>55--64.</li>
+
+<li>
+Gelfand, A. E. and Sahu, S. K. (1999) <a href="papers/ident.html"> 
+Identifiability, improper priors, and Gibbs sampling for generalized linear 
+models. </a> 
+<i>Journal of the American Statistical Association. </i><b>94,
+</b>247--253.</li>
+
+<li>
+Gilks, W. R., Roberts, G. O. and Sahu, S. K. (1998) <a href="papers/gilksetal98.pdf">
+Adaptive Markov Chain Monte Carlo through Regeneration. </a> 
+<i>Journal of the American Statistical Association, </i><b>93,
+</b>1045--1054.</li>
+
+<li>
+Roberts, G. O. and Sahu, S. K. (1997) Updating Schemes, Correlation Structure,
+Blocking and Parameterisation for the Gibbs Sampler. <i>Journal of the
+Royal Statistical Society, </i>B, <b>59,
+</b>291--317.</li>
+
+<li>
+Sahu, S. K., Dey, D. K., Aslanidou, H. and Sinha, D. (1997) A Weibull Regression
+Model with Gamma Frailties for Multivariate Survival Data. <i>Lifetime
+Data Analysis, </i><b>3, </b>123--137.</li>
+
+
+<li> Gelfand, A. E., Sahu, S. K. and Carlin, B. P. (1996) 
+Efficient parametrizations for generalized linear mixed models,
+          (with discussion). 
+          In <i>  Bayesian Statistics 5 </i>, J.M. Bernardo, J.O. Berger,
+           A.P. Dawid and A.F.M. Smith, Oxford:  Oxford University Press,
+          pp. 165--180.
+
+<li> Gelfand, A. E., Sahu, S. K. and Carlin, B. P. (1995) 
+Efficient parametrizations for normal linear mixed models. 
+           <i> Biometrika, </i> <b> 82, </b> 479--488.
+
+
+<li> Dey, D. K., Kuo, L. and Sahu, S. K. (1995) 
+A Bayesian Predictive Approach to Determining the Number of
+           Components in a Mixture Distribution <i> Statistics 
+           and Computing, </i>  <b> 5 </b>, 297--305.  
+
+<li> Gelfand, A. E. and  Sahu, S. K.  (1994) 
+On Markov Chain Monte Carlo Acceleration. 
+   <i> Journal of Computational and Graphical Statistics </i> 
+  <b> 3, </b>  261--276.  
+               
+<li> Sahu, S. K., Bendel, R. B. and Sison, C. P. (1993)
+Effect of Relative Risk and Cluster Configuration on the Power of the 
+          One-dimensional Scan Statistic.
+         <i> Statistics in Medicine, </i>  <b> 12, </b>  1853--1865.    
+              
+<li> Mukhopadhyay, N., Chattopadhyay, S. and Sahu, S. K. (1993)
+Further Developments in Estimation of the Largest Mean of $K$ Normal
+ Populations. <i> Metrika, </i> <b>  40, </b>  173--183.
+
+</ol>
+
+<h1> <b>Discussion and Invited Comments</b></h1>
+
+<ol> 
+
+
+<li> Sahu, S. K. (2009)
+<a href="papers/jasa2010comment.pdf"> 
+Comment on the paper "A Moving Average Approach for Spatial Statistics Models of Stream Networks", by J. M. Ver Hoef and E. E. Peterson.  
+    </a> <i> Journal of the American Statistical Association </i>, <b> 105, 
+</b> 21-22. </a> 
+
+
+<li> Sahu, S. K. (2009) Comments on "Approximate Bayesian Inference for latent
+Gaussian models using integrated nested Laplace Approximations" by Rue, 
+Martino and Chopin.  <i>Journal of the
+  Royal Statistical Society, </i>B. <b> 71, </b> 358--359. 
+
+<li> Sahu, S. K. (2009) Report on the spatial statistics 
+meeting held in Southampton on June 19, 2009.  <i> RSS News, </i>
+<b> 37, </b>  Number 2, pp 9. 
+
+
+<li> Gelfand, A. E. and Sahu, S. K. (2005) Comments 
+on ``On Model Expansion, Model Contraction, Identifiability and 
+Prior Information: Two Illustrative scenerios Involving 
+Mis-measured Variables" by Paul Gustafson. 
+ <i> Statistical Science. </i> 
+<b> 20,</b> 130-131. 
+</li>
+
+<li> Sahu, S. K. and Mardia, K. V. (2004) Comments on the paper
+"A conditional approachfor multivariatextreme values" by 
+Heffernan, J. E.  and Tawn, J. A. 
+<i> 
+Journal of the
+Royal Statistical Society, </i> B, <b> 66, </b> 536.  
+
+
+<li> Sahu, S. K. (2002) Comments on the paper "Bayesian measures of model complexity and fit" 
+by Spiegelhalter, D., Best, Carlin and van der  Linde. 
+<i> Journal of the
+Royal Statistical Society, </i> B, <b> 64 </b> 625--626. 
+
+
+<li> Sahu, S. K. (2002) Review of the book Analysis of Multivariate Survival Data by P. Hougaard. 
+ <i> Biometrics </i> 
+<b> 58</b> 259. 
+
+<li> Sahu, S. K.  (2001) Review of the book 
+<i> Monte Carlo Methods in Bayesian Computation </i> by 
+Chen, M.-H., Shao, Q.-M. and Ibrahim, J. G. <i> Biometrics </i> 
+<b> 57,</b> 326. </li>
+
+<li> Sahu, S. K. (2000) Comments on the paper "Time series analysis of non-Gaussian observations based on state space models from both classical and Bayesian perspectives" by Durbin, J  and Koopman, S. J. 
+ <i>Journal of the
+Royal Statistical Society, </i>B, <b>62,
+</b>35--36.</li>
+
+<li> 
+Roberts, G. O. and Sahu, S. K. (1997) 
+Discussion of the paper "The EM Algorithm--An Old Folk-Song Sung to a Fast New Tune" by Meng, X.-L. and van Dyk, D.
+<i> Journal of the Royal Statistical Society,</i>  B, 
+       <b> 59</b>, 558--559. </li>
+
+
+<li> Sahu, S. K. and Gelfand A. E.  (1996) Comment on 
+``Convergence of Markov Chain Monte Carlo Algorithms"
+          In <i>  Bayesian Statistics 5 </i>, J.M. Bernardo, J.O. Berger,
+           A.P. Dawid and A.F.M. Smith, Oxford:  Oxford University Press,
+          pp. 316--317.
+
+<li> Roberts, G. O., Sahu, S. K. and Gilks, W. R. (1995) 
+    Comment on ``Bayesian Computation and Stochastic Systems".
+     <i> Statistical Science, </i>
+         <b> 10, </b> 49--51.   
+
+</ol>
+
+<?
+my_Footer('/research/');
+?>
+
+
+
+
+
+
+
+
+
