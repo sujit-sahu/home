491591c67c0fa433dfb1f84bb232ec23cc3fb118
Fly-20 <project@fly-20.com>
2025-04-30 17:48:36 +0100

    Clearing existing

--- out/_next/static/build-1745403433684/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fapi\u002Fchapters\u002F[bookId]\u002F[chapterId]","\u002Fbooks\u002F[bookId]","\u002Fbooks\u002F[bookId]\u002F[chapterId]","\u002Fbooks\u002F[bookId]\u002Fchap[chapterId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- view.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-hugo server --disableFastRender --i18n-warnings
-
-
