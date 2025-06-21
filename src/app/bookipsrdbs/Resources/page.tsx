ad8d41b97ecaeb09ca9e767352c676f684ab718f
Prof Sujit Sahu <S.K.Sahu@soton.ac.uk>
2025-06-21 18:34:25 +0100

    powerpoint file removed.

--- src/app/bookipsrdbs/Resources/page.tsx
+++ src/app/bookipsrdbs/Resources/page.tsx
@@ -53,9 +53,7 @@ const Resources: NextPage = () => {
                     </p>
 
                     <p className="text-gray-600 mb-6">
-                      Here are my <a href="/soton_tester.pdf" className="text-blue-600 hover:text-blue-800">statistics (pdf version)</a> or 
-                      <a href="/statistics_talk.pptx" className="text-blue-600 hover:text-blue-800"> powerpoint slides</a> 
-                      for my talk on 8/9/2024.
+                      Here are my <a href="/soton_tester.pdf" className="text-blue-600 hover:text-blue-800">slides for a statistics seminar.</a>  
                     </p>
                   </div>
                 </div>
