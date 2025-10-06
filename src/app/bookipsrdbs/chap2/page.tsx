1393c6737b1d1e359e3de901479de181fa796526
Prof Sujit Sahu <S.K.Sahu@soton.ac.uk>
2025-10-06 17:35:39 +0100

    Update page.tsx with vignette link

--- src/app/bookipsrdbs/chap2/page.tsx
+++ src/app/bookipsrdbs/chap2/page.tsx
@@ -8,7 +8,8 @@ export default function Chapter2() {
       This chapter introduces the R software package and discusses how to get started with many examples. It revisits some of the data sets already mentioned in Chapter 1 by drawing 
         simple graphs and obtaining summary statistics. The data sets are also provided in <a href="/ipsRdbsdata.zip" className="text-blue-600 hover:text-blue-800">this zip file. </a> 
          </p>
-
+             Here is {' '}
+                  <a href="/vignette_full.html" className="text-blue-600 hover:text-blue-800"> a R package vignette file  to get started </a>.
 
       <div className="my-8 flex flex-col md:flex-row md:flex-wrap gap-4 md:gap-8 not-prose">
         <Image
